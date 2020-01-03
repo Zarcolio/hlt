f6a2d976331d2159d5b1e7e862d1f4a2e4986515
Ocraz <registered@quicknet.nl>
2020-01-03 20:29:48 +0100

    Add files via upload

--- 2ulb.py
+++ 2ulb.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
 import os
 
-
 if len(sys.argv) == 1:
-    print
-    print " Too few arguments:"
-    print ' script2userbin <inputfile>'
-    print 
+    print ("\n")
+    print ("Too few arguments:")
+    print ("2ulb.py <inputfile> [-f]")
+    print ("\n")
     sys.exit(2)
-    
-scriptname = sys.argv[1]
 
-chmodcmd = "chmod +x " + scriptname
+if len(sys.argv) == 3:
+    sForce = sys.argv[2]
+else:
+    sForce = ""
+
+scriptname = sys.argv[1]
+scriptfullpath = os.path.abspath(scriptname)
+
+chmodcmd = "chmod +x " + scriptfullpath
 retval1 = os.system(chmodcmd)
 
-scriptpath = os.path.abspath(scriptname)
-scriptname = os.path.splitext(scriptname)[0]
-      
-lncmd = "ln -s " + "\"" + scriptpath + "\"" + " \"/usr/local/bin/" + scriptname + "\"" 
+scriptfilename = os.path.split(scriptfullpath)[1]
+scriptname = os.path.splitext(scriptfilename)[0]
+
+lncmd = "ln " + sForce + " -s " + "\"" + scriptfullpath + "\"" + " \"/usr/local/bin/" + scriptname + "\"" 
 retval2 = os.system(lncmd)
