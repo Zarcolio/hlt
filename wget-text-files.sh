eea7d0656986f93c705280ad1c45a0bb18b5d251
Zarcolio <registered@quicknet.nl>
2020-05-07 15:45:52 +0200

    Update wget-text-files.sh

--- wget-text-files.sh
+++ wget-text-files.sh
@@ -1,1 +1,1 @@
-wget -nd -r $1 -q -E -R woff,jpg,gif,eot,ttf,svg,png,otf,pdf,exe,zip,rar,tgz,docx
+wget -nd -r $1 -q -E -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64;rv:75.0) Gecko/20100101 Firefox/75.0" -R woff,jpg,gif,eot,ttf,svg,png,otf,pdf,exe,zip,rar,tgz,docx
